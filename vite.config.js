ae719be83a018b5b964abec97d8765d3c8e1fae9
Developer <developer@enterprise-pm.com>
2026-08-12 10:39:36 +0530

    Allow Render host in vite preview config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: true,
+  },
 })
+
 
